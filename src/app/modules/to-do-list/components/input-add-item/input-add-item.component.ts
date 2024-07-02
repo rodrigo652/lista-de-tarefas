e7b281ff25a0690c91bb7cef5fdbafd0f0079751
rodrigo652 <rodrigo.camargo52@outlook.com>
2024-07-02 17:03:49 -0300

    lista de tarefas em angular

---
+++ src/app/modules/to-do-list/components/input-add-item/input-add-item.component.ts
@@ -0,0 +1,41 @@
+import { ChangeDetectorRef, Component, ElementRef, EventEmitter, Input, Output, ViewChild, inject, input } from '@angular/core';
+
+// Interfaces
+import { IListItems } from '../../interface/IListItems.interface';
+import { NgClass } from '@angular/common';
+
+
+@Component({
+  selector: 'app-input-add-item',
+  standalone: true,
+  imports: [NgClass],
+  templateUrl: './input-add-item.component.html',
+  styleUrl: './input-add-item.component.scss'
+})
+export class InputAddItemComponent {
+  #cdr = inject(ChangeDetectorRef);
+
+  @ViewChild("inputText") public inputText!: ElementRef;
+
+  @Input({required: true}) public inputListItems: IListItems[] = []
+  @Output() public outputAddListItem = new EventEmitter<IListItems>()
+  public focusAndAddItem(value: string) {
+    if(value) {
+      this.#cdr.detectChanges();
+      this.inputText.nativeElement.value = '';
+
+      const currentDate = new Date();
+      const timestamp = currentDate.getTime();
+      const id = `ID ${timestamp}` 
+
+
+      this.outputAddListItem .emit({
+        id,
+        checked: false,
+        value
+      })
+      
+      return this.inputText.nativeElement.focus();
+    }
+  }
+}
